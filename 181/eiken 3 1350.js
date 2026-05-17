0061993eccc0e92dfab46d105906bc9abdee2be7
gakus-house <29yy6m6qb6@privaterelay.appleid.com>
2026-05-17 17:20:32 +0900

    Add files via upload

---
+++ 181/eiken 3 1350.js
@@ -0,0 +1,10 @@
+const eikenData = [
+{word:"go",meaning:"",pos:"動詞"},
+{word:"get",meaning:"",pos:"動詞"},
+{word:"take",meaning:"",pos:"動詞"},
+{word:"make",meaning:"",pos:"動詞"},
+{word:"buy",meaning:"",pos:"動詞"},
+{word:"see",meaning:"",pos:"動詞"},
+{word:"look",meaning:"",pos:"動詞"},
+{word:"call",meaning:"",pos:"動詞"},
+];
